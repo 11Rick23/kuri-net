e61d451895ffe3dc1acacd27713ae7b08253c9d8
11Rick23 (MBA) <RM.11Rick23@gmail.com>
2025-11-14 11:49:32 +0900

    レスポンシブデザイン対応
    画面サイズに応じてホームページのテキストのサイズを調整するように変更

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,22 @@ export default function Home() {
 		<main>
 			<Header />
 			<div className="flex items-center justify-center w-screen h-screen flex-col gap-10">
-				<h1 className="text-9xl animate-float">ようこそ！</h1>
-				<p className="text-7xl animate-float">Welcome to kuri-kuri.net</p>
+				<h1
+					className="
+				text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl
+				animate-float
+				"
+				>
+					ようこそ！
+				</h1>
+				<p
+					className="
+				text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 
+				animate-float
+				"
+				>
+					Welcome to kuri-kuri.net
+				</p>
 			</div>
 		</main>
 	);
