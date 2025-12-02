3353eee72e5ddb7b81653d382a1e15d37d8dcec1
11Rick23 (MBA) <RM.11Rick23@gmail.com>
2025-12-02 22:08:45 +0900

    オーバーレイのz-indexを修正

--- app/tools/pdf-merge/components/dropOverlay.tsx
+++ app/tools/pdf-merge/components/dropOverlay.tsx
@@ -6,7 +6,7 @@ export default function DropOverlay({ isDraggingPDF }: Props) {
 	return (
 		<div
 			className={`
-        fixed flex items-center justify-center w-screen h-screen
+        fixed flex items-center justify-center w-screen h-screen z-10
         ${isDraggingPDF ? "bg-blue-600/80" : "bg-red-600/80"}
         `}
 		>
