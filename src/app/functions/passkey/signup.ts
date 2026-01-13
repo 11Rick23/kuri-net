6745a7d251eff3f338b4afd34f29ea9b6f2e0fd7
11Rick23 (MBA) <RM.11Rick23@gmail.com>
2026-01-14 00:05:50 +0900

    登録プロセスでのチャレンジ生成とID生成を一つの関数に統合

--- src/app/functions/passkey/signup.ts
+++ src/app/functions/passkey/signup.ts
@@ -1,5 +1,5 @@
 import { checkPasskeySupport } from "./checkPasskeySupport";
-import { generateChallenge, generateUserId } from "./server";
+import { generateRegistrationData } from "./generateRegistrationData";
 
 export default async function signUp(userName: string) {
 	// パスキーがサポートされているか確認
@@ -14,25 +14,58 @@ export default async function signUp(userName: string) {
 	console.log("パスキーはサポートされているようです。");
 
 	// チャレンジとユーザーIDを取得
-	const encodedChallenge = await generateChallenge();
-	const challenge = Buffer.from(encodedChallenge, "base64url");
-	const userId = await generateUserId();
+	const { encodedChallenge, userId } = await generateRegistrationData();
+	const challenge = base64urlToUint8Array(encodedChallenge);
 
 	// パスキーの作成を開始
-	navigator.credentials.create({
-		publicKey: {
-			challenge: new Uint8Array(challenge),
-			rp: { name: "kuri-net" },
-			user: {
-				id: new TextEncoder().encode(userId),
-				name: userName,
-				displayName: userName,
+	try {
+		const credentials = await navigator.credentials.create({
+			publicKey: {
+				challenge: challenge,
+				rp: { name: "kuri-net" },
+				user: {
+					id: new TextEncoder().encode(userId),
+					name: userName,
+					displayName: userName,
+				},
+				pubKeyCredParams: [
+					{ type: "public-key", alg: -8 }, // Ed25519
+					{ type: "public-key", alg: -7 }, // ES256
+					{ type: "public-key", alg: -257 }, // RS256
+				],
 			},
-			pubKeyCredParams: [
-				{ type: "public-key", alg: -8 }, // Ed25519
-				{ type: "public-key", alg: -7 }, // ES256
-				{ type: "public-key", alg: -257 }, // RS256
-			],
-		},
-	});
+		});
+		if (!isPublicKeyCredential(credentials)) {
+			alert("パスキーの作成に失敗しました。");
+			console.error("クレデンシャルが取得できませんでした。");
+			return;
+		}
+
+		console.log("パスキーの作成に成功しました: \n", credentials.id);
+	} catch (error) {
+		alert("パスキーの作成中にエラーが発生しました。");
+		console.error("パスキーの作成エラー:", error);
+		return;
+	}
+}
+
+function base64urlToUint8Array(base64url: string) {
+	let base64 = base64url.replace(/-/g, "+").replace(/_/g, "/");
+	const pad = base64.length % 4;
+	if (pad) base64 += "=".repeat(4 - pad);
+
+	const bin = atob(base64);
+	const bytes = new Uint8Array(bin.length);
+	for (let i = 0; i < bin.length; i++) bytes[i] = bin.charCodeAt(i);
+	return bytes;
+}
+
+function isPublicKeyCredential(x: unknown): x is PublicKeyCredential {
+	return (
+		typeof x === "object" &&
+		x !== null &&
+		"type" in x &&
+		(x as Record<string, unknown>).type === "public-key" &&
+		"rawId" in x
+	);
 }
