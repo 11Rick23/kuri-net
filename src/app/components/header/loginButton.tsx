459d02f101cbdbd6ee35b0507a1a9d22bdf84233
11Rick23 (MBA) <RM.11Rick23@gmail.com>
2026-01-19 18:41:02 +0900

    WebAuthn非対応時のエラーメッセージを作成

--- src/app/components/header/loginButton.tsx
+++ src/app/components/header/loginButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { browserSupportsWebAuthn } from "@simplewebauthn/browser";
 import Link from "next/link";
-
 import { IoMdLogIn } from "react-icons/io";
 import { useModal } from "@/app/components/modal/modalProvider";
 import RegistrationModalContent from "@/app/components/registration/registrationModal";
@@ -11,33 +11,46 @@ export default function LogInButton() {
 	const { toast, dismiss } = useToast();
 	const { openModal } = useModal();
 
-	const message = (
-		<>
-			{"新規アカウント登録は "}
-			<Link
-				href=""
-				className="underline font-medium hover:text-blue-600 dark:hover:text-blue-400"
-				onClick={() => {
-					dismiss("sign-up-notice");
-					openModal(<RegistrationModalContent />, { paddingSize: 6 });
-				}}
-			>
-				こちら
-			</Link>
-		</>
-	);
+	function onLoginButtonPress() {
+		if (!browserSupportsWebAuthn()) {
+			const message = (
+				<>
+					{
+						"ご利用中のブラウザはパスキー認証（WebAuthn）に対応していません。別のブラウザをご利用ください。"
+					}
+				</>
+			);
+			toast(message, { type: "error", durationMs: 5000 });
+			return;
+		}
+
+		const message = (
+			<>
+				{"新規アカウント登録は "}
+				<Link
+					href=""
+					className="underline font-medium hover:text-blue-600 dark:hover:text-blue-400"
+					onClick={() => {
+						dismiss("sign-up-notice");
+						openModal(<RegistrationModalContent />, { paddingSize: 6 });
+					}}
+				>
+					こちら
+				</Link>
+			</>
+		);
+		toast(message, {
+			id: "sign-up-notice",
+			type: "info",
+			durationMs: 20000,
+		});
+	}
 
 	return (
 		<button
 			type="button"
 			aria-label="ログイン"
-			onClick={() => {
-				toast(message, {
-					id: "sign-up-notice",
-					type: "info",
-					durationMs: 20000,
-				});
-			}}
+			onClick={onLoginButtonPress}
 			className="
 				w-7 h-7 rounded-full cursor-pointer
 				flex items-center justify-center
