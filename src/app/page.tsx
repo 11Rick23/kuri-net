28dc8973a6e82c0ab8bdc7d15cc88c1a2a168939
11Rick23 (MBA) <RM.11Rick23@gmail.com>
2026-01-26 23:19:19 +0900

    ホームページにログインで変化する要素を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,42 @@
 import type { Metadata } from "next";
+import LiveClock from "@/components/liveClock";
+import { verifySession } from "@/functions/passkey/verifySession";
 
 export const metadata: Metadata = {
 	title: "ホームページ",
 };
 
-export default function Home() {
+export default async function Home() {
+	const session = await verifySession();
+
+	const loggedOutContent = {
+		title: "こんにちは！",
+		description: (
+			<>
+				kuri-kuri.net へようこそ。
+				<br />
+				ヘッダーの一番右にあるログインボタンから
+				<br />
+				アカウントの登録やログインができます。
+			</>
+		),
+	};
+
+	const loggedInContent = {
+		title: "お帰りなさい！",
+		description: <LiveClock />,
+	};
+
 	return (
 		<main>
 			<div className="flex items-center justify-center w-screen h-screen flex-col gap-10">
-				<h1
-					className="
-				text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl
-				animate-float
-				"
-				>
-					ようこそ！
+				<h1 className="text-[clamp(0rem,10vw,30rem)] text-center animate-float">
+					{session?.userID ? loggedInContent.title : loggedOutContent.title}
 				</h1>
-				<p
-					className="
-				text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 
-				animate-float
-				"
-				>
-					Welcome to kuri-kuri.net
+				<p className="text-[clamp(0rem,3vw,10rem)] text-center">
+					{session?.userID
+						? loggedInContent.description
+						: loggedOutContent.description}
 				</p>
 			</div>
 		</main>
